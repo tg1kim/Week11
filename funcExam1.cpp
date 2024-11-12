fc0dee722e06ae4f8377da0ff2b2bbe388e6a669
tg1kim <ktg@kangnam.ac.kr>
2024-11-13 08:10:34 +0900

    Add files via upload

---
+++ funcExam1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#pragma warning(disable: 4996 4326 6031)
+
+void printStars()
+{
+	for (int i = 0; i < 30; i++)
+		putchar('*');
+}
+
+void main()
+{
+	printStars();
+	printf("\nHello World!\n");
+	printStars();
+}
