77308bed0f4589fdb679ad62dc2059d05890eb23
tg1kim <ktg@kangnam.ac.kr>
2024-11-13 19:26:49 +0900

    Add files via upload

---
+++ lotto1A.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>		// ·£´ý
+
+#pragma warning(disable: 4996 4326 6031)
+
+void main()
+{
+	for (int i = 0; i < 6; i++)
+		printf("%d ", rand());
+	putchar('\n');
+}
